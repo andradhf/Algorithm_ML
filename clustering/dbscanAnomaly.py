6a436ad0c69381ef3402748152d4009a43186298
Andradhf <dhafa71110@gmail.com>
2024-06-26 09:37:59 +0700

    update : learning dbscans anomaly detection

--- clustering/dbscanAnomaly.py
+++ clustering/dbscanAnomaly.py
@@ -19,7 +19,7 @@ scaler = MinMaxScaler()
 x_scaled = scaler.fit_transform(x_array)
 
 #insert array to dbscans
-dbscans = DBSCAN(eps=0.2, min_samples=3)
+dbscans = DBSCAN(eps=0.1, min_samples=5)
 dbscans.fit(x_scaled)
 
 #menampilkan jumlah cluster
@@ -28,10 +28,13 @@ n_raw = len(labels)
 n_clusters_ =len(set(labels)) - (1 if -1 in labels else 0)
 print("Terdapat "+ str(n_clusters_) + " Cluster yang terbentuk")
 
+a = int(input("Masukan nomor cluster yang ingin di lihat(0-" +str(n_clusters_ -1)+", tekna -1 jika ingin melihat data tak wajar):"))
+
 #show cluster data
-for i in range(1, 163):
-    print("id records : " + str(driver.values[i, 0]) + " id Androdi : " + str(driver.values[i, 1]) + ", Cluster : " + str(dbscans.labels_[i]))
-    print("-------------------------------------------------------------------------------------------------------------")
+for i in range(0, n_raw):
+    if(dbscans.labels_[i]==a):
+        print("id records : " + str(driver.values[i, 0]) + " id Androdi : " + str(driver.values[i, 1]) + ", Cluster : " + str(dbscans.labels_[i]))
+        print("-------------------------------------------------------------------------------------------------------------")
 
 #showing data visualisation
 driver["kluster"] = dbscans.labels_
