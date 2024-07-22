0129140be4049844679b1d43b8913627e54a8931
Andradhf <dhafa71110@gmail.com>
2024-07-22 19:36:20 +0700

    update: tugas clustering

--- clustering/dbscanAnomaly.py
+++ clustering/dbscanAnomaly.py
@@ -5,41 +5,48 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import MinMaxScaler
 
-driver = pd.read_csv("go_track_tracks.csv")
-print(driver)
+# Load data
+card = pd.read_csv("CreditCardTransaction.csv", delimiter=";")
 
-#selecting attribute
-driver_x = driver.drop(["id", "id_android", "time", "rating", "rating_bus", "rating_weather", "car_or_bus", "linha"], axis=1)
+# Convert TranxDate column to TranxDatetime
+card['TranxDate'] = pd.to_datetime(card['TranxDate'])
 
-#change data into array
-x_array = np.array(driver_x)
+# Select attributes (excluding non-numeric columns)
+card_x = card.drop(["Department", "Division", "Year", "Month", "Merchant", "TranxDescription", "TranxDate"], axis=1)
 
-#normalisation process
+# Convert TranxDate to numeric (number of days since epoch)
+card['TranxDate'] = (card['TranxDate'] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
+card_x['TranxDate'] = card['TranxDate']
+
+# Change data into array
+x_array = np.array(card_x)
+
+# Normalization process
 scaler = MinMaxScaler()
 x_scaled = scaler.fit_transform(x_array)
 
-#insert array to dbscans
-dbscans = DBSCAN(eps=0.1, min_samples=5)
+# Insert array to DBSCAN
+dbscans = DBSCAN(eps=0.1, min_samples=6)
 dbscans.fit(x_scaled)
 
-#menampilkan jumlah cluster
-labels=dbscans.labels_
+# Menampilkan jumlah cluster
+labels = dbscans.labels_
 n_raw = len(labels)
-n_clusters_ =len(set(labels)) - (1 if -1 in labels else 0)
-print("Terdapat "+ str(n_clusters_) + " Cluster yang terbentuk")
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+print("Terdapat " + str(n_clusters_) + " Cluster yang terbentuk")
 
-a = int(input("Masukan nomor cluster yang ingin di lihat(0-" +str(n_clusters_ -1)+", tekna -1 jika ingin melihat data tak wajar):"))
+a = int(input("Masukan nomor cluster yang ingin di lihat (0-" + str(n_clusters_ - 1) + ", tekan -1 jika ingin melihat data tak wajar): "))
 
-#show cluster data
-for i in range(0, n_raw):
-    if(dbscans.labels_[i]==a):
-        print("id records : " + str(driver.values[i, 0]) + " id Androdi : " + str(driver.values[i, 1]) + ", Cluster : " + str(dbscans.labels_[i]))
+# Show cluster data
+for i in range(0, 500):
+    if dbscans.labels_[i] == a:
+        print("idobservasi : " + str(card.values[i, 0]) + " Nukleocapsid : " + str(card.values[i, 1]) + " Spike : " + str(card.values[i, 2]) + ", Cluster : " + str(dbscans.labels_[i]))
         print("-------------------------------------------------------------------------------------------------------------")
 
-#showing data visualisation
-driver["kluster"] = dbscans.labels_
-output = plt.scatter(x_scaled[:, 0], x_scaled[:, 1], s=100, c=driver.kluster, marker="o", alpha=1,)
-plt.title("Hasil Klustering DBscans")
+# Showing data visualization
+card["kluster"] = dbscans.labels_
+output = plt.scatter(x_scaled[:, 0], x_scaled[:, 1], s=100, c=card.kluster, marker="o", alpha=1,)
+plt.title("Hasil Klustering DBSCAN")
 plt.colorbar(output)
 plt.legend()
 plt.show()
